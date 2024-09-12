3efdc282bca7c5b49560f4fc375af64d345e08c0
Andrezinrc <andrem.33333@gmail.com>
2024-09-12 05:09:28 -0300

    Adiciona funcionalidade de upload de arquivos e tratamento de erro

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var upload = multer({ dest: 'uploads/' });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Nemhum arquivo enviado' });
+  }
 
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
-
+// Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
